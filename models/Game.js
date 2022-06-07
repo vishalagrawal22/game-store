2b008799401310c8471eb907fe3853b30d07d04e
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-06-07 18:33:22 +0530

    Add Game model

---
+++ models/Game.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+  name: { type: String, required: true, maxlength: 100 },
+  description: String,
+  imageURL: { type: String, default: "/images/default-game-image.png" },
+  price: { type: Number, min: 0, default: 0 },
+  quantity: { type: Number, min: 0, default: 0 },
+  categories: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+  ageRating: {
+    type: String,
+    required: true,
+    enum: ["PEGI 3", "PEGI 7", "PEGI 12", "PEGI 16", "PEGI 18"],
+  },
+  supportedDevices: [{ type: Schema.Types.ObjectId, ref: "Device" }],
+});
+
+gameSchema.virtual("url").get(function () {
+  return "/games/" + this._id;
+});
+
+const Game = mongoose.model(gameSchema);
+
+export default Game;
