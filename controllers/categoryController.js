a26f42bcde5ee4b783d81e22fae52ff98f615bdc
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-06-09 03:36:31 +0530

    Add category update view

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -87,13 +87,67 @@ export const postCategoryCreate = [
   },
 ];
 
-export function getCategoryUpdate(req, res) {
-  res.send("Not Implemented: GET Category Update");
+export function getCategoryUpdate(req, res, next) {
+  async.parallel(
+    {
+      categories: (cb) => Category.find().select("name").exec(cb),
+      category: (cb) => Category.findById(req.params.id).exec(cb),
+    },
+    (err, { categories, category }) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (category === null) {
+        res.redirect("/categories");
+      }
+
+      res.render("category-form", {
+        categories,
+        category,
+      });
+    }
+  );
 }
 
-export function postCategoryUpdate(req, res) {
-  res.send("Not Implemented: POST Category Update");
-}
+export const postCategoryUpdate = [
+  body("name")
+    .trim()
+    .isLength({ min: 3, max: 100 })
+    .withMessage("name should be 3-100 characters long.")
+    .escape(),
+  body("description").trim().escape(),
+  (req, res, next) => {
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      Category.find()
+        .select("name")
+        .exec((err, categories) => {
+          if (err) {
+            return next(err);
+          }
+          res.render("category-form", {
+            categories,
+            category,
+            errors: errors.array(),
+          });
+        });
+    } else {
+      Category.findByIdAndUpdate(req.params.id, category, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/categories");
+      });
+    }
+  },
+];
 
 export function getCategoryDelete(req, res) {
   res.send("Not Implemented: GET Category Delete");
